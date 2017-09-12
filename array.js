c5179b9702544c6c579974333b458b18cd8d1e88
Alex Law <achlawuk@gmail.com>
2017-09-12 12:09:27 +0100

    Update printReverse() solution

--- array.js
+++ array.js
@@ -1,1 +1,11 @@
-alert("array.js");
+
+function printReverse(arr) {
+  for(var i = arr.length - 1; i >= 0; i--) {
+    console.log(arr[i]);
+  }
+}
+
+console.log("printReverse([1,2,3,4])")
+printReverse([1,2,3,4]);
+console.log("printReverse([\"a\",\"b\",\"c\"])")
+printReverse(["a","b","c"]);
