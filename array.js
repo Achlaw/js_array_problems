47a4893976b3ea7c9032809bb872039caf86f533
Alex Law <achlawuk@gmail.com>
2017-09-12 18:22:56 +0100

    Update isUniform() solution

--- array.js
+++ array.js
@@ -1,3 +1,4 @@
+// printReverse
 
 function printReverse(arr) {
   for(var i = arr.length - 1; i >= 0; i--) {
@@ -9,3 +10,26 @@ console.log("printReverse([1,2,3,4])")
 printReverse([1,2,3,4]);
 console.log("printReverse([\"a\",\"b\",\"c\"])")
 printReverse(["a","b","c"]);
+
+// ifUniform
+
+function isUniform(arr){
+  var first = arr[0];
+  for(var i = 1; i < arr.length; i++) {
+    if(arr[i] !== first){
+      console.log(false);
+      return false;    
+    }
+  }
+  console.log(true);
+  return true;
+}
+
+console.log("isUniform([\"a\",\"b\",\"c\"])")
+isUniform(["a","b","c"]);
+
+console.log("[1,1,1,1]");
+isUniform([1,1,1,1]);
+
+console.log("[2,1,1,1]");
+isUniform([2,1,1,1]);
